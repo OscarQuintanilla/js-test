7743539784e9dddbfcbed621ad8b6e245d526efa
oscarquintanilla <oscar.david.quintanilla@gmail.com>
2023-02-17 12:33:51 -0600

    Added remove function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // JS Code here...
+function removeRow(id){
+  let fila = document.getElementById(id)
+  fila.parentNode.removeChild(fila)
+}
+
 document.getElementById('submit').addEventListener("click", function () {
   var id = Math.floor((Math.random() * 1000) + 1);
   var product = document.getElementById('product').value
@@ -6,7 +11,7 @@ document.getElementById('submit').addEventListener("click", function () {
   var iva = document.getElementById('iva').value
 
   let buffer = document.getElementById('new-product').innerHTML;
-  document.getElementById('new-product').innerHTML = buffer + `<tr>
+  document.getElementById('new-product').innerHTML = buffer + `<tr id=${id}>
     <td>${id}</td>
     <td>${product}</td>
     <td>${price}</td>
@@ -14,7 +19,7 @@ document.getElementById('submit').addEventListener("click", function () {
     <td>${Number(Number(price) * (Number(iva) / 100)) + Number(price)}</td>
     <td>${(new Date()).toDateString()}</td>
     <td>
-      <button class="btn btn-delete">Delete</button>
+      <button class="btn btn-delete" onclick="removeRow(${id})">Delete</button>
     </td>
   </tr>`
 })
